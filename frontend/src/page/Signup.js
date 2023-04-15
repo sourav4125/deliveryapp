4ddb6ba3986e38e88ff56d40a31c6e6c14af1d71
sourav4125 <souravmisra4125@gmail.com>
2023-04-15 22:31:27 +0530

    fsoc

--- frontend/src/page/Signup.js
+++ frontend/src/page/Signup.js
@@ -55,7 +55,7 @@ console.log(process.env.REACT_APP_SERVER_DOMIN)
     if (firstName && email && password && confirmPassword) {
       if (password === confirmPassword) {
           console.log(data)
-          const fetchData = await fetch("http://localhost:8080/signup",{
+          const fetchData = await fetch("https://deliveryapp-gray.vercel.app/signup",{
             method : "POST",
             headers : {
               "Content-Type" : "application/json"
